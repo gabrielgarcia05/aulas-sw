ce4a455b3cc5e021ac99758aa7ded36787e1faac
Gabriel Garcia <100931438+gabrielgarcia05@users.noreply.github.com>
2023-03-28 11:02:22 -0300

    Métodos Construtores
    aula do dia 28/03/2023

---
+++ MetodoConstrutor/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MetodoConstrutor;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa("FULANO", 23);
+        // p1.InsereNome("GABRIEL");
+        // p1.InsereSobreNome("GARCIA DA SILVA");
+        // p1.InsereIdade(16);
+
+        p1.MostraDados();
+    }
+}
---
+++ TicToc/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TicToc;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto p1 = new Produto("ABC", 10);
+        p1.MostraDados();
+    }
+}
+
+
+//SEM VALOR ALGUM
+// --> Produto p1 = new Produto();
+//Nome: TIC TOC
+//Preco: 15
+
+//XYZ
+// --> Produto p1 = new Produto(XYZ);
+//Nome: XYZ
+//Preco: 10
+
+//ABC, 10
+// --> Produto p1 = new Produto("ABC", 10);
+//Nome: ABC
+//Preco: 9
+
